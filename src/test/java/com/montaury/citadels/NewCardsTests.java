5e1b44da37daef5320b4fa1b1621ab7a6734c397
Guillaume Grimault <ggrimault@iutbayonne.univ-pau.fr>
2020-03-20 18:46:29 +0100

    Modification finale avant rendu, reglage de pb de tests

--- src/test/java/com/montaury/citadels/NewCardsTests.java
+++ src/test/java/com/montaury/citadels/NewCardsTests.java
@@ -43,7 +43,7 @@ public class NewCardsTests {
         Group group = new Group(player1, Character.MAGICIAN);
         CardPile pioche = new CardPile(List.of(WATCHTOWER_1, WATCHTOWER_3, WATCHTOWER_2));
 
-        ActionType.PICK_2_CARDS.getAction().executeAction(group, pioche, null);
+        ActionType.PICK_2_CARDS.getAction().executeAction(group, pioche, null, null);
     //ActionType.RECEIVE_2_COIN.getAction().executeAction(group, pioche, groups);
         assertThat(player1.getCards().size()).isEqualTo(2);
     }
@@ -56,7 +56,7 @@ public class NewCardsTests {
         firstCity.buildDistrict(HOSPICE); // pour la forme
         Group group = new Group(player1, Character.MAGICIAN);
 
-        ActionType.RECEIVE_1_COIN.getAction().executeAction(group, null, null);
+        ActionType.RECEIVE_1_COIN.getAction().executeAction(group, null, null, null);
 
         assertThat(player1.getGold()).isEqualTo(1);
     }
